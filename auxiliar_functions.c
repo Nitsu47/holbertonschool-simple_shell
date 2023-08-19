4822cdff72f914c70f90f9891f8cbcc156021c1d
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-08-19 20:18:03 -0300

    betty style update in auxiliar_functions.c

--- auxiliar_functions.c
+++ auxiliar_functions.c
@@ -5,68 +5,68 @@
 */
 int _strtok(void)
 {
-    char *buffer = malloc(1024);
-    char *token;
-    size_t leng = 1024;
+char *buffer = malloc(1024);
+char *token;
+size_t leng = 1024;
 
-    while (1)
-    {
-        printf("$ ");
-        getline(&buffer, &leng, stdin);
-        printf("%s", buffer);
-    }
-    token = strtok(buffer, "");
+while (1)
+{
+	printf("$ ");
+	getline(&buffer, &leng, stdin);
+	printf("%s", buffer);
+}
+token = strtok(buffer, "");
 
-    if(token == NULL)
-    {
-        return (-1);
-    }
-    while (token)
-    {
-        printf("%s", token);
-        token = strtok(NULL, " ");
-    }
-    return (0);
+if(token == NULL)
+{
+	return (-1);
+}
+while (token)
+{
+	printf("%s", token);
+	token = strtok(NULL, " ");
+}
+return (0);
 }
 /**
 * tokenize - Splits a string
 * @buffer:the pointer to input string
 * @x: the delimiter for strtok
 * Return: A pointer to an array of tokens.
- **/
+**/
 char **tokenize(char *buffer, const char *x)
 {
-	char *token = NULL, **commands = NULL;
-	size_t bufsize = 0;
-	int i = 0;
+char *token = NULL, **commands = NULL;
+size_t bufsize = 0;
+int i = 0;
+	
+if (buffer == NULL)
+	return (NULL);
 
-	if (buffer == NULL)
-		return (NULL);
-
-	bufsize = _strlen(buffer);
-	commands = malloc((bufsize + 1) * sizeof(char *));
-	if (commands == NULL)
+bufsize = _strlen(buffer);
+commands = malloc((bufsize + 1) * sizeof(char *));
+if (commands == NULL)
+{
+	perror("Unable to allocate buffer");
+	free(buffer);
+	free(commands);
+	exit(EXIT_FAILURE);
+}
+	
+token = strtok(buffer, x);
+while (token != NULL)
+{
+	commands[i] = malloc(_strlen(token) + 1);
+	if (commands[i] == NULL)
 	{
 		perror("Unable to allocate buffer");
-		free(buffer);
 		free(commands);
-		exit(EXIT_FAILURE);
+		return (NULL);
 	}
-
-	token = strtok(buffer, x);
-	while (token != NULL)
-	{
-		commands[i] = malloc(_strlen(token) + 1);
-		if (commands[i] == NULL)
-		{
-			perror("Unable to allocate buffer");
-			free(commands);
-			return (NULL);
-		}
-		_strcpy(commands[i], token);
-		token = strtok(NULL, x);
-		i++;
-	}
+	_strcpy(commands[i], token);
+	token = strtok(NULL, x);
+	i++;
+}
 	commands[i] = NULL;
 	return (commands);
 }
@@ -77,15 +77,14 @@ char **tokenize(char *buffer, const char *x)
 **/
 void _EOF(char **buffer)
 {
-	if (*buffer)
-	{
-		free(*buffer);
-		*buffer = NULL;
-	}
-
-	if (isatty(STDIN_FILENO))
-	{
-		printf("\n");
-	}
-		exit(EXIT_SUCCESS);
+if (*buffer)
+{
+	free(*buffer);
+	*buffer = NULL;
+}
+if (isatty(STDIN_FILENO))
+{
+	printf("\n");
+}
+	exit(EXIT_SUCCESS);
 }
