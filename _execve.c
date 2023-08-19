f82e25ff20dd8021292c699bb51f1def9e0be472
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-08-19 20:29:09 -0300

    update, betty code style added and trailing whitespaces removed in _execve.c

--- _execve.c
+++ _execve.c
@@ -1,33 +1,34 @@
 #include "shell.h"
 /**
 * _execve - executes commands passed.
+* Return: executed command.
 */
 int exec(char **args)
 {
-	pid_t pid;
-    int status;
+pid_t pid;
+int status;
 
-    pid = fork();
-    if (pid == 0)
-    {
-        if (execvp(args[0], args) == -1)
-        {
-            perror("Execution error");
-            return (-1);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("Fork error");
-        return (-1);
-    }
-    else
-    {
-    do 
+pid = fork();
+if (pid == 0)
+{
+	if (execvp(args[0], args) == -1)
 	{
-        waitpid(pid, &status, WUNTRACED);
-    } 
+		perror("Execution error");
+		return (-1);
+	}
+}
+else if (pid < 0)
+{
+	perror("Fork error");
+	return (-1);
+}
+else
+{
+	do
+	{
+		waitpid(pid, &status, WUNTRACED);
+	}
 	while (!WIFEXITED(status));
-    }
- 	return (1);
+}
+	return (1);
 }
