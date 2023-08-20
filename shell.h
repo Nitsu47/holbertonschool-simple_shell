a683fffa5d721afed858999d78299ff9d09e15dd
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-08-19 21:26:11 -0300

    betty style update in shell.h

--- shell.h
+++ shell.h
@@ -19,11 +19,12 @@ void _EOF(char **buffer);
 int fork2(void);
 char **_getPATH(char **env);
 int getppid(void);
-int maxpid();
+int maxpid(void);
 int pid(void);
 unsigned int _strlen(char *s);
 void print_env(void);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 char **tokenize(char *buffer, const char *x);
+
 #endif
