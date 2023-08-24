7b215e334a4b78cb749cda4f0774f53949d3d629
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-08-24 01:53:12 -0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,20 +9,22 @@ char *command = NULL;
 size_t len = 1024;
 ssize_t read;
 
-while (_EOF(&command) == 0)
+while (1)
 {
-printf("$ ");
-fflush(stdout);
-read = getline(&command, &len, stdin);
-if (read == -1)
-{
-perror("getline");
-continue;
-}
-if (command[read - 1] == '\n')
-command[read - 1] = '\0';
+    printf("$ ");
+    fflush(stdout);
+    read = getline(&command, &len, stdin);
+    if (read == -1)
+    {
+        perror("getline");
+        break;
+    }
+    if (_EOF(&command))
+        break;
+    if (command[read - 1] == '\n')
+        command[read - 1] = '\0';
 
-printf("%s\n", command);
+    printf("%s\n", command);
 }
 free(command);
 return (0);
