69180f338d7da851af8d70fba79391ea5893704d
Nitsu47 <135637506+Nitsu47@users.noreply.github.com>
2023-08-24 02:34:41 -0300

    changes for betty style in shell.c

--- shell.c
+++ shell.c
@@ -11,20 +11,23 @@ ssize_t read;
 
 while (1)
 {
-    printf("$ ");
-    fflush(stdout);
-    read = getline(&command, &len, stdin);
-    if (read == -1)
-    {
-        perror("getline");
-        _EOF(&command);
-    }
-    if (read == 1 && command[0] == '\n')
-        _EOF(&command);
-    if (command[read - 1] == '\n')
-        command[read - 1] = '\0';
-
-    printf("%s\n", command);
+printf("$ ");
+fflush(stdout);
+read = getline(&command, &len, stdin);
+if (read == -1)
+{
+perror("getline");
+_EOF(&command);
+}
+if (read == 1 && command[0] == '\n')
+{
+_EOF(&command);
+}
+if (command[read - 1] == '\n')
+{
+command[read - 1] = '\0';
+}
+printf("%s\n", command);
 }
 free(command);
 return (0);
